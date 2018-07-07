c6669f5da9210f05df848b8a250bda1601944ebf
zurmuhlc <zurmuhlc@lafayette.edu>
2018-07-07 11:19:36 -0400

    performed a 1-prop z test on CLE's average viewership. Got Highly statistical evidence that there are more viewers for cleveland than on average

--- Business Analytics/BusinessAnalytics.r
+++ Business Analytics/BusinessAnalytics.r
@@ -2,8 +2,6 @@ library(sqldf)
 
 #Before pushing, comment out my working directory and uncomment yours
 
-rm(list = ls())
-
 #setwd("ProjectRepos/NBAH18/Business\ Analytics")
 setwd("/home/cameron/NBAH18/Business\ Analytics")
 
@@ -13,15 +11,40 @@ gameData <- read.csv("game_data.csv", header = T)
 playerData <- read.csv("player_data.csv", header = T)
 training<-read.csv("training_set.csv", header = T)
 
+totalViewersPerGame <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, sum("Rounded.Viewers") as Tot_Viewers from training group by Game_ID')
 
-
-totalViewersPerGame <- sqldf('select Game_ID, Game_Date, sum("Rounded.Viewers") as Tot_Viewers from training group by Game_ID')
+#Looking at the distributing of totalViewersPerGame-We see that is is heavily skewed right
+qqnorm(totalViewersPerGame$Tot_Viewers)
+qqline(totalViewersPerGame$Tot_Viewers)
+summary(totalViewersPerGame$Tot_Viewers)
+meanNumberTotalViewers <- mean(totalViewersPerGame$Tot_Viewers)
+sdTotalViewers <- sd(totalViewersPerGame$Tot_Viewers)
 
 #Seeing what dates/games were most popular. We noticed that Christmas/opening day had a large effect, as well as the caliber of the teams playing
-sqldf('select* from totalViewersPerGame order by Tot_Viewers desc') 
+#sqldf('select* from totalViewersPerGame order by Tot_Viewers desc') 
 
-#average all CLE intl viewers 
-#average intl viewers for all games
-#compare
+#Performing a z-test to see if CLE's average is higher than the overall. First getting only Cleveland games
+clevelandGames <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, Tot_Viewers from totalViewersPerGame where Home_Team = "CLE" OR Away_Team = "CLE"')
+
+#We notice that the clevelandGames$Tot_Viewers still has a right skew, but since n is so large (129), CLT applies
+qqnorm(clevelandGames$Tot_Viewers)
+qqline(clevelandGames$Tot_Viewers, main = "Normal Q-Q Plot for Cleveland's Viewership")
+
+#Gathering statistics
+sum(clevelandGames$Tot_Viewers)
+clevelandMeanViews <- sum(clevelandGames$Tot_Viewers)/length(clevelandGames$Tot_Viewers)
+clevelandN <- length(clevelandGames$Tot_Viewers)
+clevelandSd <- sd(clevelandGames$Tot_Viewers)
+
+zStarCleveland <- qnorm(.95, 0 ,1) #Obtaining critical value to compare to
+zStatCleveland <- (clevelandMeanViews - meanNumberTotalViewers)/(clevelandSd/sqrt(clevelandN)) 
+#Z statistic is so large (15)..this is compelling evidence that people watch Lebron
+
+levels(totalViewersPerGame$Home_Team)
+
+
+
+
+
 
 
