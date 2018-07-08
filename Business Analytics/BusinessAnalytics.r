a3fb4bec22f87244c2b4f2057078134da116670e
zurmuhlc <zurmuhlc@lafayette.edu>
2018-07-07 21:11:06 -0400

    Made script more readable

--- Business Analytics/BusinessAnalytics.r
+++ Business Analytics/BusinessAnalytics.r
@@ -10,7 +10,7 @@ setwd("/home/cameron/NBAH18/Business\ Analytics")
 
 #setwd("ProjectRepos/NBAH18/Business\ Analytics")
 
-#importing data
+########################################################## IMPORTING DATA ####################################################################################################
 
 gameData <- read.csv("game_data.csv", header = T)
 playerData <- read.csv("player_data.csv", header = T)
@@ -19,23 +19,21 @@ numberAllStarsPerTeam2016_2017 <- read.csv("number_all_stars_per_team_2016-2017.
 numberAllStarsPerTeam2015_2016 <- read.csv("number_all_stars_per_team_2015-2016.csv", header = T)
 testing<-read.csv("test_set.csv", header = T)
 
-head(testing)
-
 hasTopFivePlayer <- read.csv("teams_with_top_players.csv", header = T)
 
 totalViewersPerGame <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, sum("Rounded.Viewers") as Tot_Viewers from training group by Game_ID')
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-head(totalViewersPerGame)
-#Looking at the distributing of totalViewersPerGame-We see that is is heavily skewed right
+######################################## Looking at the distributing of totalViewersPerGame-We see that is is heavily skewed right#############################################
 qqnorm(totalViewersPerGame$Tot_Viewers, main = "Normal Q-Q Plot for the total viewers")
 qqline(totalViewersPerGame$Tot_Viewers)
 summary(totalViewersPerGame$Tot_Viewers)
 meanNumberTotalViewers <- mean(totalViewersPerGame$Tot_Viewers)
 sdTotalViewers <- sd(totalViewersPerGame$Tot_Viewers)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Seeing what dates/games were most popular. We noticed that Christmas/opening day had a large effect, as well as the caliber of the teams playing
-sqldf('select* from totalViewersPerGame order by Tot_Viewers desc') 
 
+######################################## Z TEST FOR CLE'S MEAN VIEWERSHIP #####################################################################################################
 #Performing a z-test to see if CLE's average is higher than the overall. First getting only Cleveland games
 clevelandGames <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, Tot_Viewers from totalViewersPerGame where Home_Team = "CLE" OR Away_Team = "CLE"')
 
@@ -53,22 +51,22 @@ zStarCleveland <- qnorm(.95, 0 ,1) #Obtaining critical value to compare to
 zStatCleveland <- (clevelandMeanViews - meanNumberTotalViewers)/(clevelandSd/sqrt(clevelandN)) 
 #Z statistic is so large (15)..this is compelling evidence that people watch Lebron
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+####################################################################### QUERIES ############################################################################################
+#Seeing what dates/games were most popular. We noticed that Christmas/opening day had a large effect, as well as the caliber of the teams playing
+sqldf('select* from totalViewersPerGame order by Tot_Viewers desc')
 
 #analyzing specific dates for total viewers
 sqldf('select* from totalViewersPerGame order by Tot_Viewers desc')
 
 #specific game query
 sqldf('select Home_Team, Away_Team from training where Game_ID = 21700015 and Game_Date = "10/19/2017"')
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
+#################################################################### FUNCTIONS AND APPENDED COLLUMNS #########################################################################
 #Convert all dates to months of the year
 totalViewersPerGame$Month <- month(as.POSIXlt(totalViewersPerGame$Game_Date, format = "%m/%d/%Y"))
-
-
-#average all CLE intl viewers 
-#average intl viewers for all games
-#compare
-gamesCLE <- sqldf('select count(*) from training where Home_Team = "CLE" OR Away_Team = "CLE"')
 
 
 
@@ -154,6 +152,7 @@ while(j < length(totalViewersPerGame$Tot_Viewers))
 }
 
 totalViewersPerGame$gameType <- as.factor(totalViewersPerGame$gameType)
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
