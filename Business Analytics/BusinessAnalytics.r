60832d18c24427034be906f1e74604ae034ec0c6
zurmuhlc <zurmuhlc@lafayette.edu>
2018-07-07 20:23:36 -0400

    Added a data set for teams with top players

--- Business Analytics/BusinessAnalytics.r
+++ Business Analytics/BusinessAnalytics.r
@@ -10,11 +10,12 @@ setwd("/home/cameron/NBAH18/Business\ Analytics")
 gameData <- read.csv("game_data.csv", header = T)
 playerData <- read.csv("player_data.csv", header = T)
 training<-read.csv("training_set.csv", header = T)
+hasTopFivePlayer <- read.csv("teams_with_top_players.csv", header = T)
 
 totalViewersPerGame <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, sum("Rounded.Viewers") as Tot_Viewers from training group by Game_ID')
 
 #Looking at the distributing of totalViewersPerGame-We see that is is heavily skewed right
-qqnorm(totalViewersPerGame$Tot_Viewers)
+qqnorm(totalViewersPerGame$Tot_Viewers, main = "Normal Q-Q Plot for the total viewers")
 qqline(totalViewersPerGame$Tot_Viewers)
 summary(totalViewersPerGame$Tot_Viewers)
 meanNumberTotalViewers <- mean(totalViewersPerGame$Tot_Viewers)
@@ -27,12 +28,12 @@ sdTotalViewers <- sd(totalViewersPerGame$Tot_Viewers)
 clevelandGames <- sqldf('select Game_ID, Home_Team, Away_Team, Game_Date, Tot_Viewers from totalViewersPerGame where Home_Team = "CLE" OR Away_Team = "CLE"')
 
 #We notice that the clevelandGames$Tot_Viewers still has a right skew, but since n is so large (129), CLT applies
-qqnorm(clevelandGames$Tot_Viewers)
-qqline(clevelandGames$Tot_Viewers, main = "Normal Q-Q Plot for Cleveland's Viewership")
+qqnorm(clevelandGames$Tot_Viewers, main = "Normal Q-Q Plot for Cleveland's Viewership")
+qqline(clevelandGames$Tot_Viewers)
 
 #Gathering statistics
 sum(clevelandGames$Tot_Viewers)
-clevelandMeanViews <- sum(clevelandGames$Tot_Viewers)/length(clevelandGames$Tot_Viewers)
+clevelandMeanViews <- mean(clevelandGames$Tot_Viewers)
 clevelandN <- length(clevelandGames$Tot_Viewers)
 clevelandSd <- sd(clevelandGames$Tot_Viewers)
 
@@ -40,7 +41,6 @@ zStarCleveland <- qnorm(.95, 0 ,1) #Obtaining critical value to compare to
 zStatCleveland <- (clevelandMeanViews - meanNumberTotalViewers)/(clevelandSd/sqrt(clevelandN)) 
 #Z statistic is so large (15)..this is compelling evidence that people watch Lebron
 
-levels(totalViewersPerGame$Home_Team)
 
 
 
